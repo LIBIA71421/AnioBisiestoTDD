997e8891ccaa287aa8158b6c8aac5786cf82c312
libia <libia.roman@ucb.edu.bo>
2023-08-22 12:04:45 +0200

    Todos los años divisibles por  400 SON años bisiestos

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,6 +1,6 @@
 
 function esBisiesto(anio){
-    if(anio === 4){
+    if(anio == 4 || anio % 400 == 0){
         return true;
     }
     return false;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -7,4 +7,11 @@ describe("Es Bisiesto", () => {
   it("El 5 no deberia considerarse como año bisiesto", () => {
     expect(esBisiesto(5)).toEqual(false);
   });
+
+  it("El 400 deberia considerarse como año bisiesto", () => {
+    expect(esBisiesto(400)).toEqual(true);
+  });
+  it("El 2000 deberia considerarse como año bisiesto", () => {
+    expect(esBisiesto(2000)).toEqual(true);
+  });
 });
